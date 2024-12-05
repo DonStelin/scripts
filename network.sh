a581e0101af828a8ad1faaba15d2dc49b41a7660
DonStelin <stelinlarios@gmail.com>
2024-12-05 17:39:46 -0600

    removed indentation

--- network.sh
+++ network.sh
@@ -10,13 +10,13 @@ sudo cat <<EOL > $CONFIG_FILE
 
 auto $INTERFACE
 iface $INTERFACE inet static
-    address 192.168.1.10
-    netmask 255.255.255.0
-    gateway 192.168.1.1
-    network 192.168.1.0
-    broadcast 192.168.1.255
-    dns-nameservers 192.168.1.100
-    dns-search unah.edu.hn
+address 192.168.1.10
+netmask 255.255.255.0
+gateway 192.168.1.1
+network 192.168.1.0
+broadcast 192.168.1.255
+dns-nameservers 192.168.1.100
+dns-search unah.edu.hn
 EOL
 
 echo "Configuración cambiada a IP estática para la interfaz $INTERFACE."

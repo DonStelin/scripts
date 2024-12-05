72d898b3fbf5125d429f71644c3a81d287a04886
DonStelin <stelinlarios@gmail.com>
2024-12-05 17:38:31 -0600

    removed dumb logic

--- network.sh
+++ network.sh
@@ -4,20 +4,9 @@ CONFIG_FILE="/etc/network/interfaces"
 
 INTERFACE="enp0s3"
 
-set_dynamic_ip() {
-    sudo cat <<EOL > $CONFIG_FILE
-# Configuración dinámica
-auto lo
-iface lo inet loopback
-EOL
-    echo "Configuración cambiada a IP dinámica."
-}
-
-set_static_ip() {
-    sudo cat <<EOL > $CONFIG_FILE
-# Configuración estática
-auto lo
-iface lo inet loopback
+sudo cat <<EOL > $CONFIG_FILE
+# auto lo
+# iface lo inet loopback
 
 auto $INTERFACE
 iface $INTERFACE inet static
@@ -29,16 +18,7 @@ iface $INTERFACE inet static
     dns-nameservers 192.168.1.100
     dns-search unah.edu.hn
 EOL
-    echo "Configuración cambiada a IP estática."
-}
 
-# Verificar configuración actual
-if grep -q "iface $INTERFACE inet static" $CONFIG_FILE; then
-    echo "Actualmente está configurado como IP estática."
-    set_dynamic_ip
-else
-    echo "Actualmente está configurado como IP dinámica o no configurado correctamente."
-    set_static_ip
-fi
+echo "Configuración cambiada a IP estática para la interfaz $INTERFACE."
 
 sudo systemctl restart networking
